d46825740de8b91bec45accdba07c9d2d5ad3191
David Strayhorn <david.strayhorn4@gmail.com>
2022-12-16 21:49:01 -0500

    set up IPC to send sql commands and replies back and forth between the main and renderer processes

--- src/main/main.ts
+++ src/main/main.ts
@@ -34,6 +34,12 @@ ipcMain.on('ipc-example', async (event, arg) => {
   event.reply('ipc-example', msgTemplate('pong'));
 });
 
+ipcMain.on('asynchronous-sql-command', async (event, sql) => {
+    db.all(sql, (err, result) => {
+        event.reply('asynchronous-sql-reply', result);
+    });
+});
+
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
   sourceMapSupport.install();
